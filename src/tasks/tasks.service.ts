9e86cc7d26c70fec82da2abfce4acaaaaf44d383
marci <marcianszlovak@gmail.com>
2020-04-11 14:00:01 +0200

    refactor CRUD functions, add task status validation pipe

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,7 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { TaskStatus } from "./task-status.enum";
 import { CreateTaskDto } from "./dto/create-task.dto";
-import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 import { TaskRepository } from "./task.repository";
-import { InjectRepository } from "@nestjs/typeorm";
 import { Task } from "./task.entity";
 
 @Injectable()
@@ -17,5 +15,24 @@ export class TasksService {
       throw new NotFoundException(`Task with ID "${id}" not found`);
     }
     return found;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto);
+  }
+
+  async deleteTask(id: number): Promise<void> {
+    const result = await this.taskRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+  }
+
+  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
+    task.status = status;
+    await task.save();
+    return task;
   }
 }
