bd698e0603253b31e135bb71735ee8b71bae93c0
marci <marcianszlovak@gmail.com>
2020-04-13 15:12:49 +0200

    add validation to AuthCredentialsDto, add password strength validator

--- src/auth/auth-credentials.dto.ts
+++ src/auth/auth-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(25)
+  @Matches(/((?=.*\d) | (?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: "Password too weak",
+  }) // pw contains at least one uppercase letter, at least one lowercase character, at least one number or special character
   password: string;
 }
