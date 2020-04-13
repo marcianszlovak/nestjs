f899a34cab0b0919c5697bf92b9c6cad37726d6a
marci <marcianszlovak@gmail.com>
2020-04-13 14:09:33 +0200

    add authmodule, user entity and userrepository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,20 @@ import { Module } from "@nestjs/common";
 import { TasksModule } from "./tasks/tasks.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeOrmConfig } from "./config/typeorm.config";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
+import { AuthModule } from './auth/auth.module';
+import { ServiceController } from './service/service.controller';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "client"),
+    }),
+    TasksModule,
+    AuthModule,
+  ],
+  controllers: [ServiceController],
 })
 export class AppModule {}
